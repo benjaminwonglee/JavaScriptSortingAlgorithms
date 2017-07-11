d54c86c9e0d99b65e689bfbf78bcae70df9eb1a4
benjaminwonglee <benjaminwonglee@gmail.com>
2017-07-11 16:51:38 +1200

    Testing capabilities of javascript in terms of variables and params.

---
+++ src/JavascriptQuicksort.js
@@ -0,0 +1,13 @@
+/**
+ * This is a simple storyboard.
+ *
+ * @returns
+ */
+function JavascriptQuicksort(nList) {
+	var pivot;
+	console.log(nList);
+	
+}
+
+
+JavascriptQuicksort([10, 2, 5, 4, 3, 8, 7, 1, 9, 6]);
