ad820825024fc345add81ee886ca685c49c130d7
benjaminwonglee <benjaminwonglee@gmail.com>
2017-07-13 00:56:47 +1200

    Completed swap function.

--- src/JavascriptQuicksort.js
+++ src/JavascriptQuicksort.js
@@ -1,7 +1,7 @@
 /**
  * 
- * This is an attempt at a quicksort algorithm written in Javascript which takes
- * an array of integers and sorts them.
+ * This is a quicksort algorithm written in Javascript which takes an array of
+ * integers and sorts them.
  * 
  * @author BenWL
  * 
@@ -13,29 +13,43 @@ function quicksortPartition(nList) {
 	}
 	
 	/*
-	 * Choose a pivot value; in this case the middle value is chosen but any
-	 * value can be chosen. Better if the approx range of the values is known;
-	 * can choose another value if the pivot is not central enough.
+	 * Choose a pivot value; in this case the last value is chosen but any value
+	 * can be chosen; but really any value can be chosen as long as it is
+	 * consistent. Better if the approx range of the values is known; since one
+	 * can choose another value if the pivot is not 'central' enough.
 	 */
-	var pivot = nList[nList.length/2];
+	var pivot = nList[nList.length - 1];
 	
 	/*
 	 * TODO: This is currently incorrect and will fix later. Sort items to be
 	 * before the pivot value and after the pivot value.
 	 */
-	var partList1 = [];
-	var partList2 = [];
+	// For left of pivot
+	var leftList = [];
+	// For right of pivot
+	var rightList = [];
+	// Iterating through elements
 	for(int i = 0; i < pivot; i++){
-		partList1[i] = nList[i];
-	}
-	for(int i = pivot - 1; i < nList.length; i++){
-		partList2[i] = nList[i];
+		if(nList[i] < pivot) {
+			swap(i, leftList, rightList)
+		}
 	}
 	quicksortPartition(partList1);
 	quicksortPartition(partList2);
 }
 
-function quicksortMerge(nList) {
+function swap(index, leftList, rightList){
+	/*
+	 * Add the element that is less than pivot to the left array. Replace the
+	 * element at the old index with the first element of the right array
+	 */
+	leftList[leftList.length] = rightList[index];
+	temp = rightList[0];
+	for(int i = 0; i < index - 1; i++){
+		rightList[i] = rightList[i+1];
+	}
+	rightList[index] = temp;
+	// Too complicated??? ArrayLists in Javascript???
 }
 
-quicksortSplit([10, 2, 5, 4, 3, 8, 7, 1, 9, 6]);
+quicksortPartition([10, 2, 5, 4, 3, 8, 7, 1, 9, 6]);
